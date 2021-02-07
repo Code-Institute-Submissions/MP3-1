4db406eae3ff877414df0c93ddd722143ccf613a
marcin-kli <klimaszewski.m@gmail.com>
2021-02-07 14:43:45 +0000

    Add show/hide functionality for user coins

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
     // hide user coins in profile page
-    $('#user_coins').hide(); 
+    $('#user_coins').hide();
+    $('#hide_coins_btn').hide();
 })
-
+// tooltiop function
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
  // show user coins in profile page
-$('#show_coins').click(function(){
-    $('#user_coins').toggle();
-    $('#show_coins').html("Hide coins");
+$('#show_coins_btn').click(function(){
+    $('#show_coins_btn').hide();
+    $('#hide_coins_btn').show();
+    $('#user_coins').show(); 
+})
+
+ // hide user coins in profile page
+$('#hide_coins_btn').click(function(){
+    $('#hide_coins_btn').hide();
+    $('#show_coins_btn').show();
+    $('#user_coins').hide(); 
 })
