56d42ebc83bc2fec4eb1ea04664c38944e2a0a6b
marcin-kli <klimaszewski.m@gmail.com>
2021-02-07 16:47:49 +0000

    Add toggle option for each coin desctiption

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
     // hide user coins in profile page
     $('#user_coins').hide();
     $('#hide_coins_btn').hide();
+
 })
+
 // tooltiop function
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
@@ -21,3 +23,18 @@ $('#hide_coins_btn').click(function(){
     $('#show_coins_btn').show();
     $('#user_coins').hide(); 
 })
+
+// show coin details
+$('#coin_link').click(function(){
+    $('.coin_details').toggle();
+})
+
+// toggle description on coin card
+// using jquery with jinja2: https://stackoverflow.com/questions/13423646/using-jquery-with-jinja2-to-provide-onclick-event-inside-a-loop-on-each-element
+$('.coin_link').each(function(){
+    var toggle_div_id = 'coin_details' + $(this).attr('id');
+    $('#'+toggle_div_id).hide();
+    $(this).click(function(){
+        $('#'+toggle_div_id).toggle();
+    });
+});
