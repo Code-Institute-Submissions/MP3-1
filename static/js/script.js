11d9d16edbf23007dadeb999b7f8cf361a786eff
marcin-kli <klimaszewski.m@gmail.com>
2021-02-12 15:14:50 +0000

    Add missing semicolons in script.js and font in style.css

--- static/js/script.js
+++ static/js/script.js
@@ -8,21 +8,21 @@ $(document).ready(function () {
     $('.count2').counter();
     // bootstrap tooltip
     $('[data-toggle="tooltip"]').tooltip(); 
-})
+});
 
  // show user coins in profile page
 $('#show_coins_btn').click(function(){
     $('#show_coins_btn').hide();
     $('#hide_coins_btn').show();
     $('#user_coins').show(); 
-})
+});
 
  // hide user coins in profile page
 $('#hide_coins_btn').click(function(){
     $('#hide_coins_btn').hide();
     $('#show_coins_btn').show();
     $('#user_coins').hide(); 
-})
+});
 
 // toggle description on coin card (profile page and home page)
 // using jquery with jinja2: https://stackoverflow.com/questions/13423646/using-jquery-with-jinja2-to-provide-onclick-event-inside-a-loop-on-each-element
@@ -55,8 +55,8 @@ $('.coin_link').each(function(){
       (deltaPositive && currentNumber >= numberTo) || (!deltaPositive &&currentNumber<= numberTo) ? currentNumber=numberTo : currentNumber;
       this.text(parseInt(currentNumber));
       currentNumber == numberTo ? clearInterval(interval1) : currentNumber;  
-    }
+    };
 
     interval1 = setInterval(changeNumber,changeTime);
-  }
+  };
 }(jQuery));
